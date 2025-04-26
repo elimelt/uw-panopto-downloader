df41cc4b025cd96a0ddedb71d906ced85091d928
Elijah Melton <elimelt@uw.edu>
2025-04-25 20:35:28 -0700

    Bump version to 1.0.7

--- uw_panopto_downloader/__init__.py
+++ uw_panopto_downloader/__init__.py
@@ -1,6 +1,6 @@
 """UW Panopto Downloader - A tool for downloading videos from UW Panopto."""
 
-__version__ = "1.0.6"
+__version__ = "1.0.7"
 
 # Check for required dependencies
 def _check_dependencies():
